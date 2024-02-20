52c71e2006e16c037a9464432ec899b1e5fc123d
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-02-20 21:57:51 +0100

    feat: add tv shows and poster

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,29 +18,43 @@ export default {
     },
 
     methods: {
-        // AXIOS GET METHOD
+
+        searchAllMedia() {
+            this.searchMedia();
+            this.searchMediaTv();
+        },
+
+        // AXIOS GET MOVIE METHOD
         searchMedia() {
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=' + this.apiKey + this.searchedMedia).then((response) => {
                 store.movies = response.data.results
             })
         },
 
+        // AXIOS GET TV
+        searchMediaTv() {
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=' + this.apiKey + this.searchedMedia).then((response) => {
+                store.tvs = response.data.results
+                console.log(response.data.results)
+            })
+        },
 
+        // FLAGS
         langFlag(flag) {
 
             if (flag == 'it') {
 
                 return "fi fi-it";
 
-            } else if (flag == 'en') {
+            };
+            
+            if (flag == 'en') {
 
                 return "fi fi-gb"
 
-            }
+            };
 
-        }
-
-
+        },
 
     },
 
@@ -60,35 +74,65 @@ export default {
 
             <div>
                 <input type="text" v-model="searchedMedia">
-                <button @click="searchMedia()">SEARCH</button>
+                <button @click="searchAllMedia()">SEARCH</button>
             </div>
         </div>
     </nav>
 
     <main>
-        <ul v-for="movie in store.movies">
-            <li>Titolo: {{ movie.title }}</li>
+        <!-- MOVIES -->
+        <div>
+            <h2>Movies</h2>
+            <ul v-for="movie in store.movies">
+                <li>Titolo: {{ movie.title }}</li>
 
-            <li>Titolo: Originale {{ movie.original_title }}</li>
+                <li>Titolo Originale: {{ movie.original_title }}</li>
 
-            <li>
-                Lingua: <span :class="langFlag(movie.original_language)"></span>
-            </li>
+                <li>
+                    Lingua: <span :class="langFlag(movie.original_language)"></span>
+                </li>
 
-            <li>Voto: {{ movie.vote_average }}</li>
-        </ul>
+                <li>Voto: {{ movie.vote_average }}</li>
+
+                <li>Poster:
+                    <div>
+                        <img :src="'https://image.tmdb.org/t/p/w342/' + movie.poster_path" alt="">
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h2>Tv Shows</h2>
+            <ul v-for="tv in store.tvs">
+                <li>Titolo: {{ tv.name }}</li>
+
+                <li>Titolo Originale: {{ tv.original_name }}</li>
+
+                <li>
+                    Lingua: <span :class="langFlag(tv.original_language)"></span>
+                </li>
+
+                <li>Voto: {{ tv.vote_average }}</li>
+
+                <li>Poster:
+                    <div>
+                        <img :src="'https://image.tmdb.org/t/p/w342/' + tv.poster_path" alt="">
+                    </div>
+                </li>
+            </ul>
+        </div>
+
     </main>
 </template>
 
 <!-- STYLE -->
 <style lang="scss" scoped>
-.nav-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: black;
-}
-
-// <span class="fi fi-"></span>
+    .nav-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: black;
+    }
 
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,4 +4,5 @@ export const store = reactive({
     apiUri: '...',
     apiKey: '39564388e6f5898659130a45021226e9&query=',
     movies: [],
+    tvs:[],
 })
